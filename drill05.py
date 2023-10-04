817f1cc343cba9aee64a0bbea6df824c98c61327
wonju <jwwon00@naver.com>
2023-10-04 19:34:48 +0900

    반복적으로 만들어지는 랜덤 손 화살표 위치로 캐릭터 이동 구현 완료. 값이 1을 넣으면 다시, 새로운 손 화살표가 랜덤 위치에 만들어짐.

--- drill05.py
+++ drill05.py
@@ -36,10 +36,16 @@ def reset_world():
     frame = 0
     action = 3
 
+    set_new_target_arrow()
+
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
     sx, sy = cx, cy  # p1 : 시작점
-    #hx, hy = 50, 50
+    # hx, hy = 50, 50
     hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2 : 끝점
     t = 0.0
+
 
 def render_world():
     clear_canvas()
@@ -62,6 +68,10 @@ def update_world():
         cx = (1 - t) * sx + t * hx  # cx는 시작 x와 끝 x를 1-t : t 의 비율로 섞은 위치
         cy = (1 - t) * sy + t * hy
         t += 0.001
+    else:
+        cx, cy = hx, hy # 캐릭터 위치를 목적지 위치와 강제로 정확히 일치시킴
+        set_new_target_arrow()
+
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 load_resources()
@@ -69,8 +79,8 @@ hide_cursor()
 reset_world()
 
 while running:
-    render_world()  # 월드의 현재내용을 그린다.
-    handle_events()  # 사용자의 입력을 받아들인다.
-    update_world()  # 월드안의 객체들의 상호작용을 계싼하고 그 결과를 update한다.
+    render_world()  # 월드의 현재 내용을 그린다.
+    handle_events()  # 사용자 입력을 받아들인다.
+    update_world()  # 월드 안의 객체들의 상호작용을 계싼하고 그 결과를 update한다.
 
 close_canvas()
