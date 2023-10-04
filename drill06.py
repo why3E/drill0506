9b1ffec33981bcf15f56440cba3b6756b9bb00ec
wonju <jwwon00@naver.com>
2023-10-04 20:43:23 +0900

    reformat

--- drill06.py
+++ drill06.py
@@ -22,8 +22,8 @@ def handle_events():  # 종료 입력 처리 함수
             running = False
         elif event.type == SDL_MOUSEMOTION:
             mx, my = event.x, TUK_HEIGHT - 1 - event.y
-        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT: # 마우스 클릭이 있으면
-            points.append((event.x,TUK_HEIGHT - 1 - event.y)) # 클릭된 위치를 새로운 점으로 추가
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:  # 마우스 클릭이 있으면
+            points.append((event.x, TUK_HEIGHT - 1 - event.y))  # 클릭된 위치를 새로운 점으로 추가
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -51,7 +51,7 @@ def set_new_target_arrow():
     global action, frame
     global target_exists
 
-    if points: # 포인트 리스트 안에 남아있는 점이 있으면,
+    if points:  # 포인트 리스트 안에 남아있는 점이 있으면,
         sx, sy = cx, cy  # p1 : 시작점
         # hx, hy = 50, 50
         hx, hy = points[0]  # p2 : 끝점
@@ -60,9 +60,10 @@ def set_new_target_arrow():
         frame = 0
         target_exists = True
     else:
-        action = 3 if action == 1 else 2 # 이전에 소년이 우측으로 이동중이였으면, IDLE 동작시 우측을 바라보도록
+        action = 3 if action == 1 else 2  # 이전에 소년이 우측으로 이동중이였으면, IDLE 동작시 우측을 바라보도록
         frame = 0
         target_exists = False
+
 
 def render_world():
     clear_canvas()
@@ -86,11 +87,11 @@ def update_world():
             cx = (1 - t) * sx + t * hx  # cx는 시작 x와 끝 x를 1-t : t 의 비율로 섞은 위치
             cy = (1 - t) * sy + t * hy
             t += 0.001
-        else: # 목표 지점에 도달했을 때
-            cx, cy = hx, hy # 캐릭터 위치를 목적지 위치와 강제로 정확히 일치시킴
-            del points[0] # 목표 지점에 왔기 떄문에 , 더 이상 필요없는 점을 삭제
+        else:  # 목표 지점에 도달했을 때
+            cx, cy = hx, hy  # 캐릭터 위치를 목적지 위치와 강제로 정확히 일치시킴
+            del points[0]  # 목표 지점에 왔기 떄문에 , 더 이상 필요없는 점을 삭제
             set_new_target_arrow()
-    elif points: # 목표 지점이 없는 상황에서, 새로운 목표 지점이 생겼을 때
+    elif points:  # 목표 지점이 없는 상황에서, 새로운 목표 지점이 생겼을 때
         set_new_target_arrow()
 
 
